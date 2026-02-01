65b07fe53122776f44e3166764d2ec5a41162a7a
Pedro <rooped83@gmail.com>
2026-02-01 23:03:54 +0100

    chore: set up Docker environment with Dockerfile and docker-compose, added .dockerignore

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -1,7 +1,7 @@
 import redis from 'ioredis';
 
  export const redisClient = new redis({
-    host: process.env.REDIS_HOST,
+    host: process.env.REDIS_HOST || 'localhost',
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD || undefined,
     enableOfflineQueue: false,
